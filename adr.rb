b91b7d3f38b11007e212f55bca6d96af72e0eb0e
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-15 13:59:02 -0300

    add new features

--- adr.rb
+++ adr.rb
@@ -2,9 +2,20 @@ puts "Bem-vindo ao cookbook, sua rede social de receitas "
 
 receitas = []
 
-puts "Digite o nome da receita: "
-receita = gets.chomp
+while (true) do
+   puts "Digite o nome da receita: "
+   receita = gets.chomp
 
-receitas << receita
+   receitas << receita
 
-puts "Receita #{receita} cadastrada com sucesso"
+   puts "Receita #{receita} cadastrada com sucesso"
+   puts
+   puts "===========RECEITAS CADASTRADAS==========="
+   # Forma de fazer o FOR em Ruby
+   # posso fazer apenas com puts receitas (nome do array)
+   receitas.each do |nome|
+      puts nome
+   
+   end
+   puts
+end
