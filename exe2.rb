7bb5d4750d0a80323e4b441d7014fc1aadef7849
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-17 07:03:36 -0300

    Explicando como deve ser o novo exercício

---
+++ exe2.rb
@@ -0,0 +1,5 @@
+# Criar um busca bilinear.
+# Consiste em fazer a busca por intervalos...
+# Primeiro buscar o elemento central do intervalo e analisar se é o elemento ou não
+# Se esse elemento for menor a busca pega o intervalo da esquerda, se maior o da direita
+# O processo se repete até encontrar o elemento correspondente.
