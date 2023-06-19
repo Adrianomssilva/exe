1ba7973001673fa823ffd3cce9e6d35985732bd3
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-19 10:52:14 -0300

    Add mais interação do usuário ao código

--- adr.rb
+++ adr.rb
@@ -2,6 +2,15 @@ puts "Bem-vindo ao cookbook, sua rede social de receitas "
 
 receitas = []
 
+# Add condicionais para dar mais opções e melhorar o cod.
+
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção"
+opcao = gets.chomp
+
 while (true) do
    puts "Digite o nome da receita: "
    receita = gets.chomp
