b40832816242f98e14f46ec061140c0d08e18914
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-20 14:58:19 -0300

    utilizando metodos para melhorar o código

--- adr.rb
+++ adr.rb
@@ -1,52 +1,58 @@
-puts "Bem-vindo ao cookbook, sua rede social de receitas "
-
-receitas = []
-
 # Add condicionais para dar mais opções e melhorar o cod.
+def menu()
+   puts "[1] Cadastrar uma receita"
+   puts "[2] Ver todas as receitas"
+   puts "[3] Sair"
+   puts ""
+   print "Escolha uma opção: "
+   return gets.to_i
+end
 
-puts "[1] Cadastrar uma receita"
-puts "[2] Ver todas as receitas"
-puts "[3] Sair"
-
-print "Escolha uma opção: "
-opcao = gets.to_i
-
-while (opcao != 3) do
-   if (opcao == 1)
+def inserir_receita (r)
    puts "Digite o nome da receita: "
    nome = gets.chomp
-    puts "Digite o tipo da receita: "
+   puts "Digite o tipo da receita: "
    tipo = gets.chomp
 
-   receitas << {nome: nome, tipo: tipo}
+   r << {nome: nome, tipo: tipo}
 
    puts "Receita #{nome} cadastrada com sucesso"
    puts
-   
-   # utilizar elsif como forma de demonstração"
+end
 
-   elsif (opcao == 2)
+def lista_receitas (r)
    puts "===========RECEITAS CADASTRADAS==========="
    # Forma de fazer o FOR em Ruby
    # posso fazer apenas com puts receitas (nome do array)
    # receitas.each do |nome|
    #    puts nome
    # end
-   receitas.each do |receitas|
+   r.each do |receitas|
    puts "#{receitas[:nome]} - #{receitas[:tipo]}"
    end
+end
+
+puts "Bem-vindo ao cookbook, sua rede social de receitas "
+
+receitas = []
+
+opcao = menu()
+
+while (opcao != 3) do
+   if (opcao == 1)
+   inserir_receita(receitas)
+   
+   # utilizar elsif como forma de demonstração"
+
+   elsif (opcao == 2)
+   lista_receitas(receitas)
    else
       puts "Opção não cadastrada"
    end
    # O menu foi add novamente para parar o loop infinito
    puts ""
    puts ""
-   puts "[1] Cadastrar uma receita"
-   puts "[2] Ver todas as receitas"
-   puts "[3] Sair"
-
-   print "Escolha uma opção: "
-   opcao = gets.to_i
+   opcao  = menu() 
 end
 
 puts "Obrigado por usar o Cookbook. Obrigado!"
