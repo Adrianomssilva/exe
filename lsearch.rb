0df8ac1ebe34d08b43f560aa01bdde2b4b6decfa
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-22 18:02:51 -0300

    testanto recriar o algoritimo

---
+++ lsearch.rb
@@ -0,0 +1,11 @@
+def l_search (array, num)
+   i = 0 
+   while i < array.size
+      return num if num == array[i]
+      i += 1
+   end
+   false
+end   
+   
+   puts l_search([1,2,3,4,5,6], 5)
+   
