2bd9336dfd0c8db53b2c1e82e2409e4ec005a7d6
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-23 16:48:32 -0300

    Boletim v0.1

---
+++ boletim.rb
@@ -0,0 +1,23 @@
+# Criar um programa que eu possa cadastrar quantos alunos eu quiser e comparar as suas médias e Buscar o aluno que eu quiser
+def cadastro ()
+   puts "Nome do Aluno : "
+   nome = gets.chomp
+   puts "Nota 1 : "
+   nota1 = gets.to_i
+   puts "Nota 2 : "
+   nota2 = gets.to_i
+   
+   return {nome: nome, nota1: nota1, nota2: nota2}
+
+end
+print "Quantos alunos serão cadastrados? "
+n_alunos = gets.to_i
+
+alunos = []
+
+
+
+n_alunos.times do 
+ alunos << cadastro()
+end
+puts alunos
