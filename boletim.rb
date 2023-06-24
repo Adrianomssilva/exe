bf7cae24a39d8feabaf0bad3d190f1be49b727de
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-24 15:32:38 -0300

    add menu

--- boletim.rb
+++ boletim.rb
@@ -1,4 +1,9 @@
 # Criar um programa que eu possa cadastrar quantos alunos eu quiser e comparar as suas médias e Buscar o aluno que eu quiser
+CADASTRAR = 1
+VERIFICAR = 2
+SAIR = 3
+
+
 def cadastro ()
    puts "Nome do Aluno : "
    nome = gets.chomp
@@ -8,8 +13,18 @@ def cadastro ()
    nota2 = gets.to_i
    media = (nota1 + nota2)/2
    return {nome: nome, nota1: nota1, nota2: nota2, media: media}
-
+#  aprender a mudar a classe do objeto para float, quero um número não inteiro (pesquisar para descobrir)
 end
+
+def menu ()
+   puts "========= BEM VINDO AO BOLETIM ESCOLAR ========="
+   puts "ESCOLHA UMA DAS OPÇÕES A BAIXO"
+   puts "[#{CADASTRAR}] CADASTRAR NOVOS ESTUDANTES"
+   puts "[#{VERIFICAR}] VERIFICAR ALUNOS CADASTRADOS"
+   puts "[#{SAIR}] SAIR"
+end
+
+
 print "Quantos alunos serão cadastrados? "
 n_alunos = gets.to_i
 
