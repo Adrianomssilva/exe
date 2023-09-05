2bf381f252beef6246e17f90dff49f69353f50ec
devadrsilva <adriano.mssilva1@gmail.com>
2023-09-05 17:18:09 -0300

    exe treinadev

--- exercicios.rb
+++ exercicios.rb
@@ -1,15 +1,9 @@
-# alunos = [{ nome: 'Alberto', nota: 7, disciplina: 
-# 'Artes' }, { nome: 'Ingrid', nota: 4, disciplina: 'Biologia' }]
+alunos = [{ nome: 'Alberto', nota: 7, disciplina: 
+'Artes' }, { nome: 'Ingrid', nota: 4, disciplina: 'Biologia' }]
 
-# n = alunos.length
-
-# n.times do |n|
-#   if alunos[n][:nota] > 5
-#     puts "o aluno #{alunos[n][:nome]} foi aprovado na disciplina #{alunos[n][:disciplina]}"
-#   else
-#     puts "o aluno #{alunos[n][:nome]} foi reprovado na disciplina #{alunos[n][:disciplina]}"
-#   end
-# end
+alunos.each do |aluno_nome|
+  puts aluno_nome[:nome]
+end
 
 
 # nota = -1
@@ -28,20 +22,20 @@
 #   puts "Insira um valor válido"
 # end 
 
-tecla_pressionada = 's'
-alunos = []
-notas = []
+# tecla_pressionada = 's'
+# alunos = []
+# notas = []
 
-while tecla_pressionada == 's' do
-  puts 'Digite o nome do aluno: '
-  nome_aluno = gets.chomp
-  puts 'Digite a nota do aluno: '
-  nota = gets.chomp
-  puts 'Digite a disciplina: '
-  disciplina = gets.chomp
-alunos << {nome: nome_aluno, nota: nota, disciplina: disciplina}
-puts alunos
-  puts 'Deseja inserir um novo aluno? s ou n'
-  tecla_pressionada = gets.chomp
+# while tecla_pressionada == 's' do
+#   puts 'Digite o nome do aluno: '
+#   nome_aluno = gets.chomp
+#   puts 'Digite a nota do aluno: '
+#   nota = gets.chomp
+#   puts 'Digite a disciplina: '
+#   disciplina = gets.chomp
+# alunos << {nome: nome_aluno, nota: nota, disciplina: disciplina}
+# puts alunos
+#   puts 'Deseja inserir um novo aluno? s ou n'
+#   tecla_pressionada = gets.chomp
 
-end
+# end
