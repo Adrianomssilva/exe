fd4749ccf436d77114c366c6569d9d44bcf47024
devadrsilva <adriano.mssilva1@gmail.com>
2023-09-05 17:14:49 -0300

    treina dev exercicios

--- exercicios.rb
+++ exercicios.rb
@@ -1,7 +1,47 @@
-a = 10
-b = 20
+# alunos = [{ nome: 'Alberto', nota: 7, disciplina: 
+# 'Artes' }, { nome: 'Ingrid', nota: 4, disciplina: 'Biologia' }]
 
-a, b = b, a
+# n = alunos.length
 
-puts a
-puts b
+# n.times do |n|
+#   if alunos[n][:nota] > 5
+#     puts "o aluno #{alunos[n][:nome]} foi aprovado na disciplina #{alunos[n][:disciplina]}"
+#   else
+#     puts "o aluno #{alunos[n][:nome]} foi reprovado na disciplina #{alunos[n][:disciplina]}"
+#   end
+# end
+
+
+# nota = -1
+# case nota
+# when 0
+#   puts 'Você tirou zero! Precisa melhorar…'
+# when 1..4
+#   puts 'Reprovado… precisa se esforçar mais...'
+# when 5
+#   puts 'Passou raspando!'
+# when 6..9
+#   puts 'Parabéns, você foi aprovado.'
+# when 10
+#   puts 'Tirou 10! Você deve ser o melhor aluno que já tive!'
+# else
+#   puts "Insira um valor válido"
+# end 
+
+tecla_pressionada = 's'
+alunos = []
+notas = []
+
+while tecla_pressionada == 's' do
+  puts 'Digite o nome do aluno: '
+  nome_aluno = gets.chomp
+  puts 'Digite a nota do aluno: '
+  nota = gets.chomp
+  puts 'Digite a disciplina: '
+  disciplina = gets.chomp
+alunos << {nome: nome_aluno, nota: nota, disciplina: disciplina}
+puts alunos
+  puts 'Deseja inserir um novo aluno? s ou n'
+  tecla_pressionada = gets.chomp
+
+end
